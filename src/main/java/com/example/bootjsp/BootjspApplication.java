c35b26a46571608a5756c1e90c801147249d923d
ruoyan <a>
2023-04-05 18:56:43 +0800

    cs

--- src/main/java/com/example/bootjsp/BootjspApplication.java
+++ src/main/java/com/example/bootjsp/BootjspApplication.java
@@ -7,11 +7,12 @@ package com.example.bootjsp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 //@Import(FdfsClientConfig.class)   //#导入FastDFS-Client组件
 @ComponentScan(basePackages = {"com.mycs,com.example.bootjsp"})
 @MapperScan(basePackages ={"com.mycs.dao"})
